09ac1e6ade545c075b5610ec83e5fb8da1f355a2
anish-lkakapragada <anish.lakkapragada@gmail.com>
2021-12-23 21:13:55 -0800

    pushy boi

--- content.js
+++ content.js
@@ -4,84 +4,66 @@
 
 const LENGTH_MINIMUM = 15;
 const DEFAULT_ALT = 'Generating Caption';
-const USELESS_PHRASES = ['No photo description available.', "Image", "Logo"]; 
+const USELESS_PHRASES = [
+	'No photo description available.',
+	'Image',
+	'Logo'
+];
 
-// add the describe.js module 
-import {describeImage} from "./describe.js";
+// add the describe.js module
+import { describeImage } from './describe.js';
 
-function useful (text) {
-  // decides whether the alt text is useful or not based on the length; 
+function useful(text) {
+	// decides whether the alt text is useful or not based on the length;
 
-  if (text.length < LENGTH_MINIMUM) {return false;} // calculate whether or not the alt text is even useful 
-  // use metric to determine this 
+	if (text.length < LENGTH_MINIMUM) {
+		return false;
+	} // calculate whether or not the alt text is even useful
+	// use metric to determine this
 
-  return true;
+	return true;
 }
 
-let images = document.getElementsByTagName('img'); // access all images
+function initialFix() {
+	let images = document.getElementsByTagName('img'); // access all images
+	for (let i = 0; i < images.length; i++) {
+		// iterate through all the images
+		let image = images[i];
 
-function initialFix () {
-  for (let i = 0; i < images.length; i++) {
-    // iterate through all the images 
-    let image = images[i];
-
-    // basically if it doesn't have sufficient alt-text
-    if (!useful(image.alt) || USELESS_PHRASES.includes(image.alt) || image.alt.length == 0 || image.alt == null || image.alt == "") {
-      // just set it to something, query later 
-      image.alt = DEFAULT_ALT;
-    }
-  }
+		console.log(`this is image alt: ${image.alt}`);
+		// basically if it doesn't have sufficient alt-text
+		if (image.alt.length < LENGTH_MINIMUM) {
+			// just set it to something, query later
+			console.log(`setting ${image.src} to default alternate text`);
+			image.alt = DEFAULT_ALT;
+		}
+	}
 }
 
-async function fix () {
-  // ayy, rate limiting time 
-  let last = new Date();
-  let requestsSent = 0; 
+async function fix() {
+	let images = document.getElementsByTagName('img'); // access all images
 
-  // 20 calls : 60 sec 
-  // 1 call: 3 sec
-  // wait 3 seconds before every call 
-
-  const promises = []; 
-  for (let i =0; i < images.length; i++) {
-      let image = images[i]; 
-      if (image.alt == DEFAULT_ALT) {
-          const timeSinceLast = new Date() - last;
-          console.log("the time since last is : " + timeSinceLast);
-          if (timeSinceLast >= 3000) {
-            console.log("OK good to go"); 
-            promises.push(describeImage(image.src).then((caption) => {
-                if (caption != null) {
-                    console.log(`new caption text : ${caption.text}`);
-                    image.alt = caption.text;
-                }
-            })); 
-          }
-
-          else {
-            // wait 
-            await new Promise(resolve => setTimeout(resolve, 3000 - timeSinceLast));
-            last = new Date(); 
-            console.log("the wait is over");
-            promises.push(describeImage(image.src).then((caption) => {
-              if (caption != null) {
-                  console.log(`new caption text : ${caption.text}`);
-                  image.alt = caption.text;
-              }
-            })); 
-          }
-       }
-    }
-
-    await Promise.all(promises); // wait till these promises resolve
+	let image;
+	const promises = [];
+	for (let i = 0; i < images.length; i++) {
+		image = images[i];
+		if (image.alt == DEFAULT_ALT) {
+			promises.push(
+				describeImage(image.src).then((caption) => {
+					image.alt = caption.text; // just set it to this caption;
+				})
+			);
+		}
+	}
 }
 
-const main = () =>  {
-  initialFix();
-  fix();
-  console.log("clean up done");
-}
+const main = () => {
+	console.log('running main again');
+	initialFix();
+	fix();
+	console.log('clean up done');
+};
 
-window.addEventListener('load', main); 
+window.addEventListener('load', main);
 
 window.onchange = main;
