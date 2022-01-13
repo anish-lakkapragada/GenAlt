aa3045f6de8c2cc7bcf0b5c92ef88b18e9e5dfa1
anish-lkakapragada <anish.lakkapragada@gmail.com>
2022-01-13 15:29:03 -0800

    renak fix this

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
 import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 
-const smelte = require('smelte/rollup-plugin-smelte');
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
@@ -18,23 +16,10 @@ function serve() {
   return {
     writeBundle() {
       if (server) return;
-      server = require('child_process').spawn(
-        'npm',
-        [
-          'run',
-          'start',
-          '--',
-          '--dev'
-        ],
-        {
-          stdio : [
-            'ignore',
-            'inherit',
-            'inherit'
-          ],
-          shell : true
-        }
-      );
+      server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+        stdio: ['ignore', 'inherit', 'inherit'],
+        shell: true
+      });
 
       process.on('SIGTERM', toExit);
       process.on('exit', toExit);
@@ -42,75 +27,59 @@ function serve() {
   };
 }
 
-
 export default [
   {
-    input   : 'popup/src/main.js',
-    output  : {
-      sourcemap : true,
-      format    : 'iife',
-      name      : 'app',
-      file      : 'popup/public/build/bundle.js'
-    },	
-    plugins : [
-      svelte({
-        compilerOptions : {
-          // enable run-time checks when not in production
-          dev : !production
-        }
-      }),
-
-      smelte({
-        purge             : production,
-        output            : 'popup/public/global.css', // it defaults to static/global.css which is probably what you expect in Sapper
-        postcss           : [], // Your PostCSS plugins
-        whitelist         : [], // Array of classnames whitelisted from purging
-        whitelistPatterns : [], // Same as above, but list of regexes
-        tailwind          : {
-          colors   : {
-            primary   : '#b027b0',
-            secondary : '#009688',
-            error     : '#f44336',
-            success   : '#4caf50',
-            alert     : '#ff9800',
-            blue      : '#2196f3',
-            dark      : '#212121'
-          },
-          darkMode : true
-        }
-      }),
-
-      // we'll extract any component CSS out into
-      // a separate file - better for performance
-      css({ output: 'bundle.css' }),
-
-      // If you have external dependencies installed from
-      // npm, you'll most likely need these plugins. In
-      // some cases you'll need additional configuration -
-      // consult the documentation for details:
-      // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    input: 'src/altify.js',
+    output: {
+      file: 'dist/altify.bundle.js',
+    }, 
+    plugins: [resolve({
+      browser: true, 
+      dedupe: ['@azure/ms-rest-js', '@azure/cognitiveservices-computervision']
+    })]
+  },
+  {
+    input: 'src/background.js', 
+    output: {
+      format: 'cjs',
+      file: 'dist/background.bundle.js'
+    }, 
+    plugins: [
       resolve({
-        browser : true,
-        dedupe  : [
-          'svelte'
-        ]
+        browser: true, 
+        dedupe: ['@azure/ms-rest-js', '@azure/cognitiveservices-computervision']
+      }),
+      production && terser(), 
+    ]
+  }, 
+  {
+    input: 'popup/src/main.js',
+    output: {
+      sourcemap: true,
+      format: 'iife',
+      name: 'app',
+      file: 'popup/public/build/bundle.js'
+    },
+    plugins: [
+      svelte({
+        compilerOptions: {
+          dev: !production
+        }
+      }),
+      css({ output: 'bundle.css' }),
+      resolve({
+        browser: true,
+        dedupe: ['svelte']
       }),
       commonjs(),
 
-      // In dev mode, call `npm run start` once
-      // the bundle has been generated
       !production && serve(),
 
-      // Watch the `public` directory and refresh the
-      // browser on changes when not in production
-      !production && livereload('popup/public'),
-
-      // If we're building for production (npm run build
-      // instead of npm run dev), minify
       production && terser()
     ],
-    watch   : {
-      clearScreen : false
-    }
+    watch: {
+      clearScreen: false
+    }, 
   }
-];
+
+]; 
