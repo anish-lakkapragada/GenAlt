f1c919b6b99072d3d33ed8044af993d466b60bdf
anish-lkakapragada <anish.lakkapragada@gmail.com>
2022-01-21 18:08:39 -0800

    functional mv2 branch for firefox

--- create.js
+++ create.js
@@ -2,7 +2,7 @@
 const { spawn } = require('child_process');
 
 const cmds = [
-  'zip -r firefox.zip images popup dist manifest.json'
+  'zip -r firefox.zip images popup dist manifest.json LICENSE'
 ]; 
 
 spawn(
