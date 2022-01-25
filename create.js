ec12263710a28919db53e64b92f66e31cd273319
anish-lkakapragada <anish.lakkapragada@gmail.com>
2022-01-24 22:47:02 -0800

    resubmitted extension 🤞

--- create.js
+++ create.js
@@ -2,7 +2,7 @@
 const { spawn } = require('child_process');
 
 const cmds = [
-  'zip -r extension.zip images popup dist manifest.json usage.html LICENSE'
+  'zip -r extension.zip images popup dist manifest.json usage.html background.bundle.js altify.bundle.js LICENSE'
 ]; 
 
 spawn(
